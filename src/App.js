c9ca1dc01dca4b0434940fb16859580053adda74
清水康司 <>
2021-01-21 08:30:54 +0900

    creaate component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 
 import React ,{Component} from 'react';
 
-function App() {
-  // const greeting = "Hi";
-  // const dom = <h1 className="foo">{greeting}</h1>
-  // return <div><h1>Hello,world</h1></div>;
+// function App() {
+//   // const greeting = "Hi";
+//   // const dom = <h1 className="foo">{greeting}</h1>
+//   // return <div><h1>Hello,world</h1></div>;
 
+//   return (
+//    <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type = "text" onChange={()=>{console.log("GGG")}} />
+//     </React.Fragment>
+//   )
+// }
+
+const App = ()=>{
   return (
-   <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type = "text" onChange={()=>{console.log("GGG")}} />
-    </React.Fragment>
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
   )
 }
+const Cat = ()=>{
+  return <div>MYA</div>
+}
+
 
 // class App extends Component{
 //   render(){

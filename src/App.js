6acabbbee1de778fa82fc8e698684ee04f8fb9e7
清水康司 <>
2021-01-20 19:57:11 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 
-
+import React ,{Component} from 'react';
 
 function App() {
-  return <div><h1>Hello,world</h1></div>;
+  // const greeting = "Hi";
+  // const dom = <h1 className="foo">{greeting}</h1>
+  // return <div><h1>Hello,world</h1></div>;
+
+  return (
+   <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type = "text" onChange={()=>{console.log("GGG")}} />
+    </React.Fragment>
+  )
 }
+
+// class App extends Component{
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello,world"
+//     )
+//   }
+// }
 
 export default App;

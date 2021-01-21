47bb8f885f2c487709d26b703748cab1531c5811
清水康司 <>
2021-01-21 21:02:37 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -15,19 +15,30 @@ import React ,{Component} from 'react';
 // }
 
 const App = ()=>{
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako",age:5},
+    {name:"Noname"},
+  ]
   return (
   <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
+    {
+      profiles.map((profile,index)=>{
+        return  <User name={profile.name} age ={profile.age} key={index}/>
+      })
+    }
+    {/* <User name={"Taro"} age ={10}/>
+    <User name={"Hanako"} age ={5}/> */}
   </div>
   )
 }
-const Cat = ()=>{
-  return <div>MYA</div>
+const User = (props)=>{
+return <div>Hi,i am {props.name},and {props.age}years old!</div>
 }
 
+User.defaultProps = {
+  age:1
+}
 
 // class App extends Component{
 //   render(){
